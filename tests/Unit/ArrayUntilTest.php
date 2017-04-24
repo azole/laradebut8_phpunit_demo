73d9f2f1e00992b0599ed2e54297bf9fb56378ed
mouson <mouson@gmail.com>
2017-04-25 04:30:19 +0800

    測試：新增第一筆測試案例

--- tests/Unit/ArrayUntilTest.php
+++ tests/Unit/ArrayUntilTest.php
@@ -5,5 +5,21 @@ use Tests\TestCase;
 
 class ArrayUntilTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function testArrayUntilExist()
+    {
+        /** Arrange */
+        $names = ['Mark', 'Duncan', 'Erica', 'Fish'];
 
+        /** Assume */
+        $expected = ['Mark'];
+
+        /** Act */
+        $actual = array_until($names, 'Duncan');
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
+    }
 }
