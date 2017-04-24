d3815b9ee8e0190ffe9f56652e691258f77b14eb
mouson <mouson@gmail.com>
2017-04-25 04:42:41 +0800

    實作：當 Array 數值不存在時丟出 exception

--- app/Helpers.php
+++ app/Helpers.php
@@ -3,6 +3,9 @@
 function array_until(array $array, $search)
 {
     $position = array_search($search, $array);
+    if ($position == false) {
+        throw new InvalidArgumentException;
+    }
 
     return array_slice($array, 0, $position);
 }
