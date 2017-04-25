bc736cfbbda5b9c7329ee9eab2cbd31e8537fc2a
mouson <mouson@gmail.com>
2017-04-25 10:24:05 +0800

    測試：新增驗證 Calculator 存在

---
+++ tests/Unit/CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Test\Unit;
+
+use Tests\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testInstanceExist()
+    {
+        /** Arrange */
+        $target = new Calculator;
+
+        /** Assume */
+
+        /** Act */
+
+        /** Assert */
+    }
+}
